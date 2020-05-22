24d186d848435bce77bf669d4cd9775023f7eb6e
Anton Kiyan <kiyananton@gmail.com>
2020-05-22 17:34:57 +0300

    Добавляет информационные страницы

--- build/js/script.js
+++ build/js/script.js
@@ -65,6 +65,35 @@
 })();
 
 (function () {
+  var navList = document.querySelector('.split-functions__nav-list');
+  var navItems = document.querySelectorAll('.split-functions__nav-item');
+  var contents = document.querySelectorAll('.split-functions__content');
+
+  var currentNavIdx = 0;
+
+  var onNavClick = function (evt) {
+    var target = evt.target;
+
+    if (
+      target.classList.contains('split-functions__nav-item-link')
+      && !target.parentNode.classList.contains('js--active')
+    ) {
+      var idx = +target.parentNode.getAttribute('data-idx');
+
+      navItems[currentNavIdx].classList.remove('js--active');
+      contents[currentNavIdx].classList.remove('js--show');
+
+      navItems[idx].classList.add('js--active');
+      contents[idx].classList.add('js--show');
+
+      currentNavIdx = idx;
+    }
+  };
+
+  navList.addEventListener('click', onNavClick);
+})();
+
+(function () {
   let scrollDownLink = document.querySelector(`.js-scroll-down`);
 
   let handleAnchorClick = function () {
